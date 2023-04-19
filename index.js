3cc061bad8d30611da375cff34377b6b3c5eb961
Braingenius2 <braingenius2@gmail.com>
2023-04-20 00:38:39 +0300

    fix eslint errors

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 // Get the hamburgerButton, closeButton and mobileMenu
-hamburgerButton = document.querySelector('#hamburger-icon');
-closeButton = document.querySelector('#menu-close');
-mobileMenu = document.getElementById('mobile-menu');
+const hamburgerButton = document.querySelector('#hamburger-icon');
+const closeButton = document.querySelector('#menu-close');
+const mobileMenu = document.getElementById('mobile-menu');
 
 // Add click event listeners to the hamburgerButton and closeButton
-hamburgerButton.addEventListener('click', function() {
-    //toggle the display of the mobile menu
-    mobileMenu.style.display = 'block';
+hamburgerButton.addEventListener('click', () => {
+  // toggle the display of the mobile menu
+  mobileMenu.style.display = 'block';
 });
-closeButton.addEventListener('click', function() {
-    //toggle the display of the mobile menu
-    mobileMenu.style.display = 'none';
+closeButton.addEventListener('click', () => {
+  // toggle the display of the mobile menu
+  mobileMenu.style.display = 'none';
 });
 
 // Add click event listeners to the mobile menu links
-mobileMenu.querySelectorAll('a').forEach(function(link) {
-    link.addEventListener('click', function() {
-      // Hide the mobile menu after a link is clicked
-      mobileMenu.style.display = 'none';
-    });
+mobileMenu.querySelectorAll('a').forEach((link) => {
+  link.addEventListener('click', () => {
+    // Hide the mobile menu after a link is clicked
+    mobileMenu.style.display = 'none';
   });
-
-
+});
