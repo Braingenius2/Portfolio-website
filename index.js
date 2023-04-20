ea264a25acb69e9f8c875b8918592c34dd577da1
Braingenius2 <braingenius2@gmail.com>
2023-04-20 15:49:55 +0300

    Define works object data in script

--- index.js
+++ index.js
@@ -20,3 +20,60 @@ mobileMenu.querySelectorAll('a').forEach((link) => {
     mobileMenu.style.display = 'none';
   });
 });
+
+let works = [
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    featuredImageMobile: 'images/Snapshoot-Portfolio-mobile.png',
+    featuredImageDesktop: 'images/Snapshoot-Portfolio-desktop.png',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript'],
+    liveVersionLink: 'https://braingenius2.github.io/Portfolio-website/project1',
+    sourceLink: 'https://github.com/Braingenius2/Portfolio-website'
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    featuredImageMobile: 'images/Snapshoot-Portfolio-mobile.png',
+    featuredImageDesktop: 'images/Snapshoot-Portfolio-desktop.png',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript'],
+    liveVersionLink: 'https://braingenius2.github.io/Portfolio-website/project1',
+    sourceLink: 'https://github.com/Braingenius2/Portfolio-website'
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    featuredImageMobile: 'images/Snapshoot-Portfolio-mobile.png',
+    featuredImageDesktop: 'images/Snapshoot-Portfolio-desktop.png',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript'],
+    liveVersionLink: 'https://braingenius2.github.io/Portfolio-website/project1',
+    sourceLink: 'https://github.com/Braingenius2/Portfolio-website'
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    featuredImageMobile: 'images/Snapshoot-Portfolio-mobile.png',
+    featuredImageDesktop: 'images/Snapshoot-Portfolio-desktop.png',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript'],
+    liveVersionLink: 'https://braingenius2.github.io/Portfolio-website/project1',
+    sourceLink: 'https://github.com/Braingenius2/Portfolio-website'
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    featuredImageMobile: 'images/Snapshoot-Portfolio-mobile.png',
+    featuredImageDesktop: 'images/Snapshoot-Portfolio-desktop.png',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript'],
+    liveVersionLink: 'https://braingenius2.github.io/Portfolio-website/project1',
+    sourceLink: 'https://github.com/Braingenius2/Portfolio-website'
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    featuredImageMobile: 'images/Snapshoot-Portfolio-mobile.png',
+    featuredImageDesktop: 'images/Snapshoot-Portfolio-desktop.png',
+    technologies: ['Ruby on Rails', 'CSS', 'Javascript'],
+    liveVersionLink: 'https://braingenius2.github.io/Portfolio-website/project1',
+    sourceLink: 'https://github.com/Braingenius2/Portfolio-website'
+  }
+];
