4af3cd8f4488c473346c7308fb24c6d5c5069c83
Braingenius2 <braingenius2@gmail.com>
2023-04-23 02:49:10 +0300

    fix eslint errors

--- index.js
+++ index.js
@@ -328,7 +328,7 @@ function validateEmail(input, requiredMsg, invalidMsg) {
   }
 
   // validate email format
-  const emailRegex = new RegExp(`^(([^<>()\\[\\]\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$`);
+  const emailRegex = new RegExp('^(([^<>()\\[\\]\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$');
 
   const email = input.value.trim();
   if (!emailRegex.test(email)) {
