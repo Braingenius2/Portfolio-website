3d94ec8ab9617567b6358653c3b2d0d38380f3bc
Braingenius2 <braingenius2@gmail.com>
2023-04-23 02:48:05 +0300

    fix eslint errors

--- index.js
+++ index.js
@@ -286,20 +286,20 @@ document.querySelectorAll('.see-project').forEach((button) => {
 
 // show a message with a type of the input
 function showMessage(input, message, type) {
-	// get the small element and set the message
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	// update the class for the input
-	input.className = type ? "success" : "error";
-	return type;
+  // get the small element and set the message
+  const msg = input.parentNode.querySelector('small');
+  msg.innerText = message;
+  // update the class for the input
+  input.className = type ? 'success' : 'error';
+  return type;
 }
 
 function showError(input, message) {
-	return showMessage(input, message, false);
+  return showMessage(input, message, false);
 }
 
 function showSuccess(input) {
-	return showMessage(input, "", true);
+  return showMessage(input, '', true);
 }
 
 function isNotLowerCase(input, message) {
@@ -310,50 +310,48 @@ function isNotLowerCase(input, message) {
 }
 
 function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
+  if (input.value.trim() === '') {
+    return showError(input, message);
+  }
+  return showSuccess(input);
 }
 
 function validateEmail(input, requiredMsg, invalidMsg) {
-	// check if the value is not empty
-	if (!hasValue(input, requiredMsg)) {
-		return false;
+  // check if the value is not empty
+  if (!hasValue(input, requiredMsg)) {
+    return false;
   }
-  
+
   if (!isNotLowerCase(input, invalidMsg)) {
     // check if the value is not in lowercase
     return false;
   }
 
-	// validate email format
-	const emailRegex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // validate email format
+  const emailRegex = new RegExp(`^(([^<>()\\[\\]\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$`);
 
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	}
-	return true;
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
 }
 
 const form = document.querySelector('#contact-me');
-const NAME_REQUIRED = "Please enter your name";
-const EMAIL_EMPTY = "Please enter your email";
-const EMAIL_INVALID = "Please enter a valid email address";
+const NAME_REQUIRED = 'Please enter your name';
+const EMAIL_EMPTY = 'Please enter your email';
+const EMAIL_INVALID = 'Please enter a valid email address';
 
-form.addEventListener("submit", function (event) {
-	// stop form submission
-	event.preventDefault();
+form.addEventListener('submit', (event) => {
+  // stop form submission
+  event.preventDefault();
 
-	// validate the form
-	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-	let emailValid = validateEmail(form.elements["email"], EMAIL_EMPTY, EMAIL_INVALID);
-	let messageValid = hasValue(form.elements["message"], "Please enter a message");
-	// if valid, submit the form.
-	if (messageValid && nameValid && emailValid) {
-		form.submit();
-	}
+  // validate the form
+  const nameValid = hasValue(form.elements.name, NAME_REQUIRED);
+  const emailValid = validateEmail(form.elements.email, EMAIL_EMPTY, EMAIL_INVALID);
+  const messageValid = hasValue(form.elements.message, 'Please enter a message');
+  // if valid, submit the form.
+  if (messageValid && nameValid && emailValid) {
+    form.submit();
+  }
 });
-
